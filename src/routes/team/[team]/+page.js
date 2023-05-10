07f1a069fdd7ba3a92f79d36d451c09dbc85f358
Doglas Rocha <79492860+DoglasRocha@users.noreply.github.com>
2023-05-10 15:27:16 -0300

    start team screen

---
+++ src/routes/team/[team]/+page.js
@@ -0,0 +1,16 @@
+import requestFromApi, { unwrapObject } from "$lib/js/fetch_func.js"
+
+export async function load({ params, fetch }) {
+
+	let results = await requestFromApi(
+		`https://ergast.com/api/f1/constructors/${params.team}/constructorStandings.json?limit=80`, fetch
+	);
+
+	results = unwrapObject(results, [], 'MRData', 'StandingsTable', 'StandingsLists');
+	let teamData = unwrapObject(results, [], 'ConstructorStandings', 'Constructor');
+
+	return {
+		results, 
+		teamData
+	}
+}
